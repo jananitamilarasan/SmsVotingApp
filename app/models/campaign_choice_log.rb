298c7417997f71832383eb3f0362560acfecb150
janani <jananirails@gmail.com>
2014-07-21 19:18:45 +0530

    message

--- app/models/campaign_choice_log.rb
+++ app/models/campaign_choice_log.rb
@@ -7,6 +7,6 @@ class CampaignChoiceLog < ActiveRecord::Base
   #### Check for invalid score #####
   scope :invalid_scores, ->(ids) { where("validity in (?)   && id in (?)",["pre","post"],ids)}
 def self.scores(ids)
-	where("id in (?)",ids)
+	where("id in (?) && validity like ?",ids,'during')
 end
 end
