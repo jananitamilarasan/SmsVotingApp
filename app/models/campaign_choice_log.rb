23e6eecb29364541a03d93d57cbc363eaa1e865a
janani <jananirails@gmail.com>
2014-07-21 19:27:14 +0530

    message

--- app/models/campaign_choice_log.rb
+++ app/models/campaign_choice_log.rb
@@ -3,10 +3,8 @@ class CampaignChoiceLog < ActiveRecord::Base
 	belongs_to :campaign
 	belongs_to :choice
 	##### Check the valid canditate score #####
-  # scope :scores, ->(ids) { where("validity like ?  && choice_id is NOT NULL && id in (?)",'during',ids)}
+  scope :scores, ->(ids) { where("validity like ?  and choice_id is NOT NULL and id in (?)",'during',ids)}
   #### Check for invalid score #####
-  scope :invalid_scores, ->(ids) { where("validity in (?)   && id in (?)",["pre","post"],ids)}
-def self.scores(ids)
-	where("id in (?) && validity = ?",ids,"during")
-end
+  scope :invalid_scores, ->(ids) { where("validity in (?)   and id in (?)",["pre","post"],ids)}
+
 end
